3b0005dc6ca48b29284dda5002b11ab81364e182
Spandan2405 <spandan2405@gmail.com>
2025-12-31 02:19:10 +0530

    Update UI#2

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="max-w-7xl mx-auto px-4 sm:px-6 py-6">
+    <main>
       <HomePage />
     </main>
   );
